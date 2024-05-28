e667432586de2f53aa22fbcc690c67ceb53acbf5
Abdeali <abdealidahodwala163@gmail.com>
2024-05-28 10:58:01 +0530

    utils -> wrapAsync for basic error handling, created

--- app.js
+++ app.js
@@ -1,76 +1,95 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const Listing = require('./models/listing');
-const path = require('path');
-const methodOverride = require('method-override')
-const ejsMate = require('ejs-mate')
+const mongoose = require("mongoose");
+const Listing = require("./models/listing");
+const path = require("path");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
-const { connectDB } = require('./connection');
-require('dotenv').config()
+const { connectDB } = require("./connection");
+const { wrapAsync } = require("./utils/wrapAsync");
+require("dotenv").config();
 
 //MongoDB connection
-connectDB().then(()=>console.log('MongoDB Connected')).catch((err)=>console.log(err))
+connectDB()
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
 
 //view engine
-app.set('view engine','ejs');
-app.set('views',path.join(__dirname,'views'))
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 //parsing middlewares
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(methodOverride('_method'))
-app.use(express.static(path.join(__dirname,'/public')))
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "/public")));
 
 //for ejs-mate
-app.engine('ejs', ejsMate);
+app.engine("ejs", ejsMate);
 
 //listing route
-app.get('/listings',async (req,res)=>{
-    const allListings = await Listing.find({});
-    res.render('listings/index',{allListings})
-})
+app.get("/listings", async (req, res) => {
+  const allListings = await Listing.find({});
+  res.render("listings/index", { allListings });
+});
 
 //create new list
-app.get('/listings/new',async (req,res)=>{
-    res.render('listings/new.ejs');
-})
+app.get("/listings/new", async (req, res) => {
+  res.render("listings/new.ejs");
+});
+
 //handling post request for create
-app.post('/listings/new',async (req,res)=>{
-    const {title,description,image,price,country,location} = req.body;
-    const newList = await Listing.create({title,description,image,price,country,location})
+app.post(
+  "/listings/new",
+  wrapAsync(async (req, res, next) => {
+    const { title, description, image, price, country, location } = req.body;
+    const newList = await Listing.create({
+      title,
+      description,
+      image,
+      price,
+      country,
+      location,
+    });
     console.log(newList);
-    res.redirect('/listings')
-})
+    res.redirect("/listings");
+  })
+);
 
 //edit get route
-app.get('/listings/:id/edit',async(req,res)=>{
-    const id = req.params.id;
-    const list = await Listing.findById(id);
-    res.render('listings/edit.ejs',{list})
-})
+app.get("/listings/:id/edit", async (req, res) => {
+  const id = req.params.id;
+  const list = await Listing.findById(id);
+  res.render("listings/edit.ejs", { list });
+});
 //handling put route
-app.patch('/listings/:id/edit',async(req,res)=>{
-    const id = req.params.id;
-    const {title,description,image,price,country,location} = req.body;
-    const editedUser = await Listing.findByIdAndUpdate({_id:id},{title,description,image,price,country,location})
-    res.redirect(`/listings/${id}`)
-})
+app.patch("/listings/:id/edit", async (req, res) => {
+  const id = req.params.id;
+  const { title, description, image, price, country, location } = req.body;
+  const editedUser = await Listing.findByIdAndUpdate(
+    { _id: id },
+    { title, description, image, price, country, location }
+  );
+  res.redirect(`/listings/${id}`);
+});
 
 //delete route
-app.delete('/listings/:id/delete',async (req,res)=>{
-    const id = req.params.id;
-    const deletedList = await Listing.findByIdAndDelete(id)
-    res.redirect('/listings')
-})
+app.delete("/listings/:id/delete", async (req, res) => {
+  const id = req.params.id;
+  const deletedList = await Listing.findByIdAndDelete(id);
+  res.redirect("/listings");
+});
 
-app.get('/listings/:id',async (req,res)=>{
-    const id = req.params.id;
-    const list = await Listing.findById(id);
-    res.render('listings/show.ejs',{list})
-})
+app.get("/listings/:id", async (req, res) => {
+  const id = req.params.id;
+  const list = await Listing.findById(id);
+  res.render("listings/show.ejs", { list });
+});
 
-
+app.use((err, req, res, next) => {
+  res.send("Something broke");
+});
 //creating new list
 // app.get('/testListing',async(req,res)=>{
 //     const newList = await Listing.create({
@@ -83,14 +102,11 @@ app.get('/listings/:id',async (req,res)=>{
 //     res.send({status:"completed"})
 // })
 
+app.get("/", (req, res) => {
+  res.send("Hey");
+});
 
-
-app.get('/',(req,res)=>{
-    res.send("Hey")
-})
-
-
-const port = process.env.PORT
-app.listen(port,()=>{
-    console.log('Server is running on port 3000 ');
-})
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log("Server is running on port 3000 ");
+});
