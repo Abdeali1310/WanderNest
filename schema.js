c3f8ef4fed8473627b69939559c4fd6de4a6a1ca
Abdeali <abdealidahodwala163@gmail.com>
2024-06-02 13:11:08 +0530

    fixed some bugs

--- schema.js
+++ schema.js
@@ -2,7 +2,7 @@ const Joi = require('joi');
 
 module.exports.listingSchema = Joi.object({
     title: Joi.string().required(),
-    description: Joi.string().min(15).required(),
+    description: Joi.string().min(15).max(100).required(),
     image: Joi.string().allow("",null).default("https://images.unsplash.com/photo-1540541338287-41700207dee6?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
     price: Joi.number().required(),
     location: Joi.string().required(),
