8ef4a0b2f7f0d26fa0cafc32abb7d1440439b9c4
Abdeali <abdealidahodwala163@gmail.com>
2024-06-12 15:23:34 +0530

    fixed category styling

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -5,6 +5,7 @@ const Listing = require("../models/listing");
 
 router.get('/:name',wrapAsync(async (req,res)=>{
     const category = req.params.name;
+    res.locals.category = category || '';
     const filteredList = await Listing.find({category:category})
     res.render('listings/index',{allListings:filteredList})
 }))
