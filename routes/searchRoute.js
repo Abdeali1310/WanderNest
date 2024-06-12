26f524f9704f3819e38b50df13a343791c35cd2f
Abdeali <abdealidahodwala163@gmail.com>
2024-06-12 15:16:51 +0530

    fixed search styling

--- routes/searchRoute.js
+++ routes/searchRoute.js
@@ -5,6 +5,7 @@ const Listing = require("../models/listing");
 
 router.get('/',wrapAsync(async (req,res)=>{
     const {listName} = req.query;
+    res.locals.search = listName || '';
     const filteredList = await Listing.find({title: new RegExp(listName, 'i')})
     res.render('listings/index',{allListings:filteredList,query: listName})
 }))
